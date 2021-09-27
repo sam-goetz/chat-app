79fa6fa0046810fb05a7b6abb55c1b24f2b878e1
Sam Goetz <sgoetz2013@gmail.com>
2021-09-27 17:34:33 -0400

    mergeed the login stuff to main branch.  Usrs can sign up and their meail displays ont ehri messages.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
-import * as rtdb from "https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-analytics.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
+import * as rtdb from "https://www.gstatic.com/firebasejs/9.1.0/firebase-database.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-analytics.js";
+import * as fbauth from "https://www.gstatic.com/firebasejs/9.1.0/firebase-auth.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,19 +21,90 @@ const firebaseConfig = {
   measurementId: "G-RTZVYWX0CV"
 };
 
-// Initialize Firebase
+// Initialize Firebase and DOM
 const app = initializeApp(firebaseConfig);
+let auth = fbauth.getAuth(app);
 const analytics = getAnalytics(app);
 
 let db = rtdb.getDatabase(app);
 let titleRef = rtdb.ref(db, "/");
 let chatsRef = rtdb.child(titleRef, 'chats');
 
+
 let inputMessage = document.getElementById('inputMessage')
 let submitChat = document.getElementById('button')
 let deleteButton = document.getElementById('clearChats')
 let listOfChats = document.getElementById('list-of-chats')
 let username = document.getElementById('username')
+
+let email = '';
+
+
+/////////////////////////////////////////////////////
+
+
+let renderUser = function (userObj) {
+  //$("#app").html(JSON.stringify(userObj));
+  $("#app").append(`<button type="button" class='btn btn-secondary' id="logout">Logout</button>`);
+  $("#logout").on("click", () => {
+    fbauth.signOut(auth);
+    console.log('signed out')
+  })
+}
+
+fbauth.onAuthStateChanged(auth, user => {
+  if (user) {
+    email = user.email;
+    console.log(email, 'is signed in')
+    $("#login-box").hide();
+    $("#app").show();
+    renderUser(user);
+  } else {
+    $("#login-box").show();
+    $("#app").hide();
+    console.log('no user detected')
+  }
+});
+
+
+
+
+$(`#register`).on(`click`, () => {
+  let email = $(`#email`).val()
+  let password = $(`#password`).val()
+
+  fbauth.createUserWithEmailAndPassword(auth, email, password).then(somedata => {
+    let uid = somedata.user.uid;
+    let userRoleRef = rtdb.ref(db, `/users/${uid}/roles/user`);
+    rtdb.set(userRoleRef, true);
+  }).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+  });
+
+})
+
+
+$('#sign-in').on('click', () => {
+  let email = $(`#email`).val()
+  let password = $(`#password`).val()
+
+  fbauth.signInWithEmailAndPassword(auth, email, password).then(
+    somedata => {
+      console.log(somedata);
+    }).catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
+
+})
+
 
 
 
@@ -40,11 +113,18 @@ let username = document.getElementById('username')
 rtdb.onValue(chatsRef, ss => {
   let allChats = ss.val();
   listOfChats.innerHTML = '';
-  //Loop through chats and append them
+  //Loop through chats and append them  
   for (const chat in allChats) {
     let displayedMessage = document.createElement('li');
+    let username = document.createElement('span');
     listOfChats.appendChild(displayedMessage)
     displayedMessage.innerText = allChats[chat].message
+
+    // console.log(allChats[chat].user)
+    username.innerText = allChats[chat].user
+    username.classList.add('chatter')
+    displayedMessage.appendChild(username)
+
     displayedMessage.id = chat;
     displayedMessage.addEventListener('dblclick', function () { testFunction(displayedMessage.id) }
 
@@ -57,7 +137,6 @@ rtdb.onValue(chatsRef, ss => {
 //rtdb.update(peopleRef,newGuy);
 
 const editDatabase = (chatID, editedMessage) => {
-  let newMessage = `{${chatID}:heyyyyo}`;
   let messageRef = rtdb.child(chatsRef, `${chatID}`);
   rtdb.update(messageRef, { 'message': editedMessage });
   //rtdb.push(chatsRef, 'hi');
@@ -86,7 +165,8 @@ const sendChat = () => {
   //let sender = username.value;
 
   let chatObject = {
-    'message': inputMessage.value
+    'message': inputMessage.value,
+    'user': email
   }
   rtdb.push(chatsRef, chatObject);
 
